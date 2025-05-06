5dec44da7c6c0340d5666d2f530df3193b34d57b
Ulrich OUSSENI COMBO <ulrichousseni@icloud.com>
2025-05-06 15:29:44 +0200

    Modification de hello.py

--- hello.py
+++ hello.py
@@ -1,7 +1,8 @@
 from colorama import init, Fore, Style
+4
 init()
 def display():
-message= f"{Fore.GREEN}Bonjour les {Fore.CYAN}FI1{Style.RESET_ALL}"
+message= f"{Fore.GREEN}Ceci est une{Fore.CYAN}modification{Style.RESET_ALL}"
 print(message)
 if __name__ == "__main__":
 display()
